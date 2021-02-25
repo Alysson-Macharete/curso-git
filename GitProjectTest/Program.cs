1b9c2710891c9fccb9611794c51ca55030070dcb
Alysson Macharete <alysson.macharete1@gmail.com>
2021-02-25 11:42:50 -0300

    Acrescentada a pergunta sobre como voce estah

--- GitProjectTest/Program.cs
+++ GitProjectTest/Program.cs
@@ -8,6 +8,8 @@ namespace GitProjectTest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
+
             Console.WriteLine();
         }
     }
